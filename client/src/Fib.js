26af5f4f7cbe5817ba8582b827cbb49f6f4a1a8f
adel <adelessam256@gmail.com>
2023-03-11 19:43:12 +0200

    docker& kubernetes full code final

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,8 +35,7 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    //return this.state.seenIndexes.map(({ number }) => number).join(', ');
-    return null;
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
